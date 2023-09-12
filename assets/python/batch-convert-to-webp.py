8bce58657388cdeec1d1b92b6671efdd6d1c46b7
david-charles-git <davidmcharles9@gmail.com>
2023-09-12 23:58:11 +0100

    adding python batch conversion executable

---
+++ assets/python/batch-convert-to-webp.py
@@ -0,0 +1,32 @@
+import os
+from PIL import Image
+
+def convert_png_to_webp(input_folder, output_folder):
+    if not os.path.exists(input_folder):
+        print(f"Input folder '{input_folder}' does not exist.")
+        return
+
+    os.makedirs(output_folder, exist_ok=True)
+    files = os.listdir(input_folder)
+
+    for file in files:
+        if file.endswith('.png'):
+            input_path = os.path.join(input_folder, file)
+            output_path = os.path.join(output_folder, os.path.splitext(file)[0] + '.webp')
+
+            try:
+                with Image.open(input_path) as img:
+                    img.save(output_path, 'webp')
+
+                print(f"Converted {input_path} to WebP and saved as {output_path}")
+            except Exception as e:
+                print(f"Failed to convert {input_path} to WebP: {e}")
+
+if __name__ == "__main__":
+    input_folder = input("Enter the path to the input folder: ")
+
+    if not os.path.exists(input_folder):
+        print(f"Input folder '{input_folder}' does not exist.")
+    else:
+        output_folder = input("Enter the path to the output folder: ")
+        convert_png_to_webp(input_folder, output_folder)
